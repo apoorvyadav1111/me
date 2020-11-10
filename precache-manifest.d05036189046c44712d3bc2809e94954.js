025cbaf222a8d7e6ac326d898c04a4978577e10a
apoorv <apoorvyadav1111@gmail.com>
2020-11-10 19:23:19 +0530

    Updates

---
+++ precache-manifest.d05036189046c44712d3bc2809e94954.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "863daada686a9524cd416210c4a2d04b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f01c912cf18a72c5171c",
+    "url": "/static/css/main.9447384d.chunk.css"
+  },
+  {
+    "revision": "64f0a16147ca8b53a5f0",
+    "url": "/static/js/2.de023be3.chunk.js"
+  },
+  {
+    "revision": "857af1c517e1f86bdb841a8a5ccc520a",
+    "url": "/static/js/2.de023be3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f01c912cf18a72c5171c",
+    "url": "/static/js/main.dc904dd9.chunk.js"
+  },
+  {
+    "revision": "9303db01b461cc5f6abb",
+    "url": "/static/js/runtime-main.db6031be.js"
+  }
+]);
